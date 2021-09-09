3518c4f9885e2555f174a7cdcaecdc960944876a
Sibints2000 <sibints2000@gmail.com>
2021-09-09 17:33:06 +0530

    Twenty First Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main v-if="!loading">
-    Show Data
+    <DataTitle :text="title" :dataDate="dataDate" />
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -12,10 +12,13 @@
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle,
+  },
   data(){
     return {
       loading: true,
