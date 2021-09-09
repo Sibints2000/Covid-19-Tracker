d88fc755ca7c08a46a29acae925686a574dd056d
Sibints2000 <sibints2000@gmail.com>
2021-09-09 17:06:12 +0530

    Twelfth Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,16 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  created() {
+    console.log()
   }
 }
 </script>
