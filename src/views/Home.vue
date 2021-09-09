c876d3b2c2269f906031a36b715803aae82d6e20
Sibints2000 <sibints2000@gmail.com>
2021-09-09 18:22:28 +0530

    Twenty Fifth Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,11 @@
 
     <DataBoxes :stats="stats" />
 
-    <CountrySelect :countries="countries" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+      Clear Country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -42,13 +46,17 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
     }
   },
   async created() {
     const data = await this.fetchCovidData()
     
     this.dataDate = data.Date
-    this.stats = data.global
+    this.stats = data.Global
     this.countries = data.Countries
     this.loading = false
   },
